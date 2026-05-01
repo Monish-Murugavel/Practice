220203f9eba4621402155127ce2a46717fb13389
Monish Murugavel <monishmurugavel0810@gmail.com>
2026-05-01 10:16:50 +0530

    Add files via upload

---
+++ Algorithms/Pattern.py
@@ -0,0 +1,18 @@
+'''i=1
+n=int(input("enter number :"))
+while i<=n:
+    print(i*"*")
+    i=i+1'''
+
+'''m=int(input("Enter number :"))
+for i in range(m):
+    print((i+1)*'*')'''
+
+l=int(input("ENter number:"))
+for i in range(l+1):
+    for j in range(i):
+        print("*",end='')
+    print()
+
+
+    
